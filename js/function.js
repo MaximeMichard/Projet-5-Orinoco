3eb2da06be47878d0b766e6eff8f8980096d0c85
Michard Maxime <maxime.michard@laposte.net>
2020-06-20 19:30:54 +0200

    modif request js

--- js/function.js
+++ js/function.js
@@ -12,3 +12,16 @@ function post(url, data) {
 
     })
 }
+function get(url) {
+    return new Promise(function (resolve) {
+        let request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+            if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+                resolve(JSON.parse(this.responseText));
+            }  
+        };
+        request.open('GET', url);
+        request.send();
+
+    })
+}
